e3e4d1d89164e7c5a45b3ba8612cddb915648e0c
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2024-09-12 22:02:11 -0300

    Update Routes.jsx

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,12 +1,23 @@
+import NavBar from '../components/navigation/NavBar';
 import Register from '../components/auth/Register';
 import Login from '../components/auth/Login';
+import Unauthorized from '../components/error/Unauthorized';
 import Error from '../components/error/Error';
+import NotFound from '../components/error/NotFound';
 import ProtectedRoute from '../security/ProtectedRouter';
 import Home from '../components/service/Home';
 
 export const appRoutes = [
     {
         path: '/',
+        element: (
+            <ProtectedRoute allowedRoles={[ 0, 1, 2, 3, 4 ]}>
+                <Home />
+            </ProtectedRoute>
+        ),
+        errorElement: <Error />,
+    }, {
+        path: '/home',
         element: (
             <ProtectedRoute allowedRoles={[ 0, 1, 2, 3, 4 ]}>
                 <Home />
@@ -22,12 +33,13 @@ export const appRoutes = [
         element: <Login />,
         errorElement: <Error />,
     }, {
-        path: '/home',
-        element: (
-            <ProtectedRoute allowedRoles={[ 0, 1, 2, 3, 4 ]}>
-                <Home />
-            </ProtectedRoute>
-        ),
-        errorElement: <Error />,
-    },
+        path: '/error',
+        element: <Error />,
+    }, {
+        path: '/unauthorized',
+        element: <Unauthorized />,
+    }, {
+        path: '/notfound',
+        element: <NotFound />,
+    }
 ];
