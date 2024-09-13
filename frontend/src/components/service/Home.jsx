dfa8bfd03168d65f085bc9ad6c2dd80693fee4cf
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2024-09-12 22:05:46 -0300

    Update Home.jsx

--- frontend/src/components/service/Home.jsx
+++ frontend/src/components/service/Home.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import NavBar from '../navigation/NavBar';
 
 function Home() {
-
-
-
     return (
-        <div className='flex flex-row h-full'>
-            <NavBar/>
-            <div className='w-full'> Home</div>
-        </div>
+        <> Home </>
     );
 };
 
