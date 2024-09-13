82a2640f76dd4cb9d38edc9f2792c8ec0cc2088f
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2024-09-12 21:58:01 -0300

    Delete backend/prisma/migrations directory

--- backend/prisma/migrations/20240910034225_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `authority` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "authority",
-ADD COLUMN     "role" INTEGER NOT NULL DEFAULT 0;
